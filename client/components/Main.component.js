c87bb3d59d49944bcebd9fd7159265d89cf4c272
Prasanna Nandalal <pnand5@sapient.com>
2017-04-08 11:15:56 +0530

    notin1

--- client/components/Main.component.js
+++ client/components/Main.component.js
@@ -10,7 +10,6 @@ class Main extends React.Component{
     render(){
         return (
         <div>
-            <h1>Login Pagesdgsdg</h1>
             {React.cloneElement(this.props.children,this.props)}
         </div>)
     }

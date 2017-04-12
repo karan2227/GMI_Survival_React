8aa4d8ddbd84d5539af60834f5b706d13c7bae53
Nehal Bhatla <nbha17@sapient.com>
2017-04-12 12:16:34 +0530

    Custom CSS

--- client/components/Login/Login.component.js
+++ client/components/Login/Login.component.js
@@ -1,49 +1,60 @@
 import React from 'react';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 
-export default class Login extends React.Component{
-    constructor(props){
+export default class Login extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={}
-      
+        this.state = {}
+
     }
-// componentDidMount(){
-//     console.log('did mount login');
-//         this.props.getStocks("http://localhost:8080/instruments");
-//         this.props.getOrders("http://localhost:8080/orders");
-// }
-    loginUserLocal(){
-        var userName=this.refs.selectedUser.value;
-         var user;
-         for(let u of this.props.users){
-           
-            if(userName === u.name){
-                
+    // componentDidMount(){
+    //     console.log('did mount login');
+    //         this.props.getStocks("http://localhost:8080/instruments");
+    //         this.props.getOrders("http://localhost:8080/orders");
+    // }
+    loginUserLocal() {
+        var userName = this.refs.selectedUser.value;
+        var user;
+        for (let u of this.props.users) {
+
+            if (userName === u.name) {
+
                 user = u;
-                
+
             }
         }
         this.props.loggedInUser(user);
     }
 
-    render(){
+    render() {
         console.log(this.props);
-        var showUserList=this.props.users.map((user,index)=> {
-            return(
-            <option value={user.name} key={index}>{user.name}</option>
+        var showUserList = this.props.users.map((user, index) => {
+            return (
+                <option value={user.name} key={index}>{user.name}</option>
             )
         })
-    
-        return(
-            <div>
-                <h1>LOGIN</h1>
-            <select ref="selectedUser">
-            {showUserList}
-            </select>
-            <Link to ="/trader">
-            <input type="button" value="LOGIN" onClick={ this.loginUserLocal.bind(this)}/>
-            </Link>
+
+        return (
+            <div className="login container-fluid">
+                <h1><b>Log In</b></h1>
+                <br/>
+                <form className="form-inline">
+                <div className="form-group">
+                    <label htmlFor="sel1">Select Trader:</label>
+                    <br/>
+                    <select className="form-control" id="sel1" ref="selectedUser">
+                        {showUserList}
+                    </select>
+                </div>
+                </form>
+                {/*<select ref="selectedUser">
+                    {showUserList}
+                </select>*/}
+                <br/>
+                <Link to="/trader">
+                    <button type="button" value="LOGIN" onClick={this.loginUserLocal.bind(this)} className="btn btn-lg loginbutton">Login</button>
+                </Link>
             </div>
         );
-        }
     }
+}

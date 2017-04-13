8ed0737555df98b336e42ba1d8e7cb258e5ac2a8
Karan Sharma 3 <ksha52@sapient.com>
2017-04-13 12:54:34 +0530

    chart complete responsive

--- client/components/Trader/Chart.component.js
+++ client/components/Trader/Chart.component.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+var Legend = require('react-d3-core').Legend;
 var BarStackHorizontalChart = require('react-d3-basic').BarStackHorizontalChart;
-import ChartLegend from './ChartLegend.component';
+//import ChartLegend from './ChartLegend.component';
 
 class Chart extends React.Component {
     render() {
 
-        var chartSeries = [
+        var legendClassName = "test-legend-class",
+            legendPosition = 'left',
+            legendOffset = 90,
+            chartSeries = [
             {
                 field: 'quantityExecuted',
                 name: 'Executed',
@@ -34,7 +37,98 @@ class Chart extends React.Component {
             xTicks=[2,'%'],
             xTickFormat = d3.format('%')
         return (
-            <div className="container-fluid row">
+            <div>
+                <div className=" hidden-xs hidden-sm  visible-md visible-lg">
+                    <h3 className="text-center">Order Execution Status</h3>
+                    <div className="center-block">
+                        <Legend
+                            legendClassName={legendClassName}
+                            legendPosition={legendPosition}
+                            legendOffset={legendOffset}
+                            chartSeries={chartSeries}
+                            width={this.props.wt * 0.3}
+                            height={this.props.ht * 0.5}
+                        />
+                    </div>
+                    <BarStackHorizontalChart
+                    data={this.props.data}
+                    chartSeries={chartSeries}
+                    width={this.props.w * 0.88}
+                    height={this.props.h * 0.8}
+                    xTicks={xTicks}
+                    showYGrid= {false}
+                    yScale={yScale}
+                    yLabel={yLabel}
+                    y={y}
+                    x={x}
+                    xTickFormat={xTickFormat}
+                    showLegend={false}
+                    className="col-xs-12"/>
+                </div>
+                <div className="visible-sm hidden-xs hidden-md hidden-lg"> 
+                    <h4 className="pull-left">Order Execution Status</h4>
+                    <div className="pull-right">
+                        <Legend
+                            legendClassName={legendClassName}
+                            legendPosition={legendPosition}
+                            legendOffset={legendOffset}
+                            chartSeries={chartSeries}
+                            width={this.props.wt * 0.3}
+                            height={this.props.ht * 0.5}
+                        />
+                    </div>
+                    <BarStackHorizontalChart
+                    data={this.props.data}
+                    chartSeries={chartSeries}
+                    width={this.props.w * 0.9}
+                    height={this.props.h * 0.8}
+                    xTicks={xTicks}
+                    showYGrid= {false}
+                    yScale={yScale}
+                    yLabel={yLabel}
+                    y={y}
+                    x={x}
+                    xTickFormat={xTickFormat}
+                    showLegend={false}
+                    className="col-xs-12"/>
+                </div>
+                 <div className="visible-xs hidden-sm hidden-md hidden-lg row"> 
+                    <h4 className="pull-left col-xs-6">Order Execution Status</h4>
+                    <div className="pull-right col-xs-6">
+                        <Legend
+                            legendClassName={legendClassName}
+                            legendPosition={legendPosition}
+                            legendOffset={legendOffset}
+                            chartSeries={chartSeries}
+                            width={this.props.wt * 0.7}
+                            height={this.props.ht * 0.7}
+                        />
+                    </div>
+                    <BarStackHorizontalChart
+                    data={this.props.data}
+                    chartSeries={chartSeries}
+                    width={this.props.w*0.99}
+                    height={this.props.h*0.8}
+                    xTicks={xTicks}
+                    showYGrid= {false}
+                    yScale={yScale}
+                    yLabel={yLabel}
+                    y={y}
+                    x={x}
+                    xTickFormat={xTickFormat}
+                    showLegend={false}
+                    className="col-xs-12"/>
+                </div>
+            </div>
+        )
+    }
+};
+
+
+export default Chart;
+/*
+//For Chart Legend Component
+<div className="container-fluid row">
                 <ChartLegend className="col-xs-12" {...this.props}/>
                 
                 <BarStackHorizontalChart
@@ -53,9 +147,4 @@ class Chart extends React.Component {
                     className="col-xs-12"/>
 
             </div>
-        )
-    }
-};
-
-
-export default Chart;
+*/ 

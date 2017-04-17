00421f7c1a4a5191b45d5b3fa34cda750c99e0dd
Neelansha Trivedi <ntriv1@sapient.com>
2017-04-17 13:17:35 +0530

    routes.js

--- client/app.js
+++ client/app.js
@@ -1,28 +1,8 @@
-
-import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
-import { Router, Route, IndexRoute, browserHistory } from "react-router";
 import css from './style/common.styl';
+import {route} from './routes.js' 
 
-import App from './components/AppList';
-import Main from './components/Main.component';
-import Login from './components/Login/Login.component';
-import Chart from './components/Trader/Chart.component';
-import Trader from './components/Trader/Trader.component';
-
-const store = configureStore();
-//here sending empty initialstates
 render(
-    <Provider store={store}>
-        <Router history={browserHistory}>
-            <Route path="/" component={App}>
-                <IndexRoute component={Login} />
-                <Route path="/trader" component={Trader}>
-                </Route>
-            </Route>
-        </Router>
-    </Provider>,
+    route,
     document.getElementById('root')
 );
---
+++ client/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+import configureStore from './store/configureStore';
+
+import App from './components/AppList';
+import Main from './components/Main.component';
+import Login from './components/Login/Login.component';
+import Chart from './components/Trader/Chart.component';
+import Trader from './components/Trader/Trader.component';
+
+ const store = configureStore();
+export var route=
+<Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={Login} />
+                <Route path="/trader" component={Trader}>
+                </Route>
+            </Route>
+        </Router>
+    </Provider>

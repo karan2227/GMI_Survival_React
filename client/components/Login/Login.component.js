1626dea03b370af54d7bb152119b4c550da30873
karan2227 <karansharma768@gmail.com>
2017-04-24 00:48:12 +0530

    Circle Progress and styling to it Implemented

--- client/components/Login/Login.component.js
+++ client/components/Login/Login.component.js
@@ -8,43 +8,74 @@ import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import config from '../../firebase/config';
 import firebase from 'firebase';
-import {hashHistory} from 'react-router';
+import { hashHistory } from 'react-router';
+import CircularProgress from 'material-ui/CircularProgress';
 
 export default class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             value: 0,
-            id: ''
+            id: '',
+            loginStatus: <h3>Please Enter your credentials</h3>
         };
 
     }
 
     loginUserLocal() {
+        //selecting user
         var userName = this.refs.selectedUser.value;
+
         var user;
         for (let u of this.props.users) {
             if (this.state.id === u.id) {
                 user = u;
             }
         }
+
+        if (user) {
+            this.setState({
+                loginStatus: <div><h3>Pleaase Wait..Logging in</h3><br /><CircularProgress size={80} thickness={5} /></div>
+            })
+        } else {
+            this.setState({
+                loginStatus: <div><h3>Please Enter valid Credentials</h3> <div className="modal-footer">
+                    <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                </div></div>
+            })
+        }
+
         this.props.loggedInUser(user);
+
+
         //firebase lOGIC
-        var email = user.id.toLowerCase().concat("@gmail.com");
-        var password = email;
+        var email = user.id.toLowerCase().concat("@gmail.com");//using id to login not user name
+        var password = email;//email is password
 
         firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(function (user) {
-                    user.updateProfile({
+            .then((user) => {
+                $('#loginStatus').modal('hide')
+                user.updateProfile({
                     displayName: userName,
                 });
+
                 hashHistory.push('/trader');
             })
-}
+            .catch(() => {
+                //error in aut{hentication
+                this.setState({
+                    loginStatus: <div><h3>Authentication Error...</h3><h3>Please Enter Valid Credentials</h3>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                })
+            })
+    }
+
     handleChange(event, index, value) {
         this.setState({ id: value });
     }
-
 
     render() {
 
@@ -53,14 +84,14 @@ export default class Login extends React.Component {
                 <MenuItem value={user.id} key={index} primaryText={user.name}></MenuItem>
             )
         })
-        //Needs to be inside css
+        //Material styling
         const styles = {
-            margin: 12
+            margin: "10%"
         }
         const style = {
-            height: 500,
+            height: "60%",
             width: "40%",
-            width:'squeeze',
+            width: 'squeeze',
             margin: 20,
             textAlign: 'center',
             display: 'inline-block',
@@ -69,42 +100,50 @@ export default class Login extends React.Component {
             marginTop: 90,
         }
         const style1 = {
-            marginTop: 100
+            marginTop: "10%"
         }
         const style2 = {
             color: "#00bcd4"
         }
-        const style3={
-            fontWeight:"bold"
+        const style3 = {
+            fontWeight: "bold"
         }
 
         return (
             <div className="container-fluid">
-                <center>
-                    <Paper className="login" style={style} zDepth={10} rounded={true}>
-                        <h1><b>LOGIN</b></h1>
-                        <br />
-                        {/*<SelectField onChange={this.handleChange.bind(this)} selectedMenuItemStyle={style2} menuItemStyle={style3} labelStyle={style3} ref="selectedUser">*/}
-                         <SelectField value={this.state.id} floatingLabelText="UserName" onChange={this.handleChange.bind(this)} selectedMenuItemStyle={style2} menuItemStyle={style3} labelStyle={style3} ref="selectedUser">
-                            {showUserList}
-                        </SelectField>
-                        <br />
-                        <TextField
-                            hintText="Password Field"
-                            floatingLabelText="Password"
-                            type="password" />
-                        <br />
-                        <br />
+                <div>
+                    <center>
+                        <Paper className="login" style={style} zDepth={10} rounded={true}>
+                            <h1><b>LOGIN</b></h1>
+                            <br />
+                            {/*<SelectField onChange={this.handleChange.bind(this)} selectedMenuItemStyle={style2} menuItemStyle={style3} labelStyle={style3} ref="selectedUser">*/}
+                            <SelectField value={this.state.id} floatingLabelText="UserName" floatingLabelStyle={{ marginLeft: '-129px', marginTop: '-10px' }} onChange={this.handleChange.bind(this)} selectedMenuItemStyle={style2} menuItemStyle={style3} labelStyle={style3} ref="selectedUser">
+                                {showUserList}
+                            </SelectField>
+                            <br />
+                            <TextField
+                                hintText="Password Field"
+                                floatingLabelText="Password"
+                                type="password" />
+                            <br />
+                            <br />
+                            <RaisedButton label="Login" primary={true} style={styles} onClick={this.loginUserLocal.bind(this)} data-toggle="modal" data-target="#loginStatus" />
 
-                            {/*<button type="button" value="LOGIN" onClick={this.loginUserLocal.bind(this)} className="btn btn-lg loginbutton">Login</button>*/}
-                            <RaisedButton label="Login" primary={true} style={styles} onClick={this.loginUserLocal.bind(this)} />
+                            <footer style={style1}>
+                                <a href="http://publicis.sapient.com/en-us.html" target="_blank"><img src={require('./Capture.PNG')} height="45" width="30" /></a>
+                                <p>Powered By <a href="http://publicis.sapient.com/en-us.html" target="_blank">Sapient</a></p>
+                            </footer>
+                        </Paper>
+                    </center>
+                </div>
+                <div className="modal fade" id="loginStatus" tabindex="-1" role="dialog">
+                    <div className="modal-dialog modal-sm" role="document">
+                        <div className="modal-content">
+                            {this.state.loginStatus}
+                        </div>
 
-                        <footer style={style1}>
-                            <a href="http://publicis.sapient.com/en-us.html" target="_blank"><img src={require('./Capture.PNG')} height="45" width="30" /></a>
-                            <p>Powered By <a href="http://publicis.sapient.com/en-us.html" target="_blank">Sapient</a></p>
-                        </footer>
-                    </Paper>
-                </center>
+                    </div>
+                </div>
             </div>
         );
     }
